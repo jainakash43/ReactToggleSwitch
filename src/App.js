1cb6bfdf38ff0096e6ad4fc6bb35f077b8ac593a
akash jain <jainakash43@gmail.com>
2020-10-31 16:21:10 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ToggleSwitch from './Components/ToggleSwitch';
 
-function App() {
+const App = () => {
+  
+  const [toggled, setToggled]= React.useState(false);
+  
+  const handleClick=()=>{
+    setToggled((s) => !s);
+  
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+     <ToggleSwitch toggled={toggled} handleClick={handleClick} />
+    
   );
-}
+};
 
 export default App;
